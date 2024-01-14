93a427fd83f2975ef6537192eb1e61682e09eea9
Matheus Borges <matob@live.com>
2024-01-14 16:22:07 -0300

    fix: fix lint issues

--- src/core/domain/value-object/Cpf.ts
+++ src/core/domain/value-object/Cpf.ts
@@ -1,70 +1,64 @@
-// @ts-nocheck
-
 export default class Cpf {
-  private value: string;
-  private DIGIT_VERIFY = 11;
-  constructor(cpf: string | null | undefined) {
-    if (!this.validate(cpf)) {
-      throw new Error('Invalid cpf');
+  private value: string
+  private DIGIT_VERIFY = 11
+  constructor (cpf?: string) {
+    if (!cpf || !this.validate(cpf)) {
+      throw new Error('Invalid cpf')
     }
 
-    this.value = cpf.replace('.', '').replace('.', '').replace('-', '').replace(' ', '');
+    this.value = cpf.replace('.', '').replace('.', '').replace('-', '').replace(' ', '')
   }
 
-  getValue() {
-    return this.value;
+  getValue () {
+    return this.value
   }
 
-  private firstDigitVerify(firstDigitVerifySum: number): number {
-    const restDivision = firstDigitVerifySum % this.DIGIT_VERIFY;
-    return restDivision < 2 ? 0 : this.DIGIT_VERIFY - restDivision;
+  private firstDigitVerify (firstDigitVerifySum: number): number {
+    const restDivision = firstDigitVerifySum % this.DIGIT_VERIFY
+    return restDivision < 2 ? 0 : this.DIGIT_VERIFY - restDivision
   }
 
-  private secondDigitVerify(secondDigitVerifySum: number, firstDigitVerify: number): number {
-    secondDigitVerifySum += 2 * firstDigitVerify;
-    const restDivision = secondDigitVerifySum % this.DIGIT_VERIFY;
-    return restDivision < 2 ? 0 : this.DIGIT_VERIFY - restDivision;
+  private secondDigitVerify (secondDigitVerifySum: number, firstDigitVerify: number): number {
+    secondDigitVerifySum += 2 * firstDigitVerify
+    const restDivision = secondDigitVerifySum % this.DIGIT_VERIFY
+    return restDivision < 2 ? 0 : this.DIGIT_VERIFY - restDivision
   }
 
-  private digitVerifyResult(firstDigitVerifySum: number, secondDigitVerifySum: number): number {
-    const firstDigitVerify = this.firstDigitVerify(firstDigitVerifySum);
-    const secondDigitVerify = this.secondDigitVerify(secondDigitVerifySum, firstDigitVerify);
-    return '' + firstDigitVerify + '' + secondDigitVerify;
+  private digitVerifyResult (firstDigitVerifySum: number, secondDigitVerifySum: number): string {
+    const firstDigitVerify = this.firstDigitVerify(firstDigitVerifySum)
+    const secondDigitVerify = this.secondDigitVerify(secondDigitVerifySum, firstDigitVerify)
+    return '' + firstDigitVerify + '' + secondDigitVerify
   }
 
-  private validate(cpf: string): boolean {
-    if (cpf == null || cpf == undefined) {
-      return false;
-    }
-
-    cpf = cpf.replace('.', '').replace('.', '').replace('-', '').replace(' ', '');
+  private validate (cpf: string): boolean {
+    cpf = cpf.replace('.', '').replace('.', '').replace('-', '').replace(' ', '')
     if (cpf.length !== this.DIGIT_VERIFY) {
-      return false;
+      return false
     }
 
-    const numbersTheSame = cpf.split('').every((c) => c === cpf[0]);
+    const numbersTheSame = cpf.split('').every((c) => c === cpf[0])
 
     if (numbersTheSame) {
-      return false;
+      return false
     }
 
-    let firstDigitVerifySum = 0;
-    let secondDigitVerifySum = 0;
-    let digito = 0;
+    let firstDigitVerifySum = 0
+    let secondDigitVerifySum = 0
+    let digito = 0
 
     for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
-      digito = parseInt(cpf.substring(nCount - 1, nCount));
-      firstDigitVerifySum = firstDigitVerifySum + (this.DIGIT_VERIFY - nCount) * digito;
-      secondDigitVerifySum = secondDigitVerifySum + (12 - nCount) * digito;
+      digito = parseInt(cpf.substring(nCount - 1, nCount))
+      firstDigitVerifySum = firstDigitVerifySum + (this.DIGIT_VERIFY - nCount) * digito
+      secondDigitVerifySum = secondDigitVerifySum + (12 - nCount) * digito
     }
 
-    const nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
-    const digitVerifyResult = this.digitVerifyResult(firstDigitVerifySum, secondDigitVerifySum);
+    const nDigVerific = cpf.substring(cpf.length - 2, cpf.length)
+    const digitVerifyResult = this.digitVerifyResult(firstDigitVerifySum, secondDigitVerifySum)
 
-    return nDigVerific == digitVerifyResult;
+    return nDigVerific === digitVerifyResult
   }
 
-  public toString() {
-    return this.getValue();
+  public toString () {
+    return this.getValue()
   }
 }
--- src/core/domain/value-object/email.ts
+++ src/core/domain/value-object/email.ts
@@ -1,11 +1,11 @@
 export default class Email {
   // TODO: criar regras do value Object
-  constructor(private value: string) {}
+  constructor (private value: string) {}
 
   /**
    * toString
    */
-  public toString(): string {
-    return this.value;
+  public toString (): string {
+    return this.value
   }
 }
